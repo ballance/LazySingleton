ccafdebaf4558a8bdfb1198aeb823d04918bb53f
Chris Ballance <cballance@boardbooks.com>
2015-05-13 17:16:48 -0400

    initial commit

---
+++ SingletonThing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    public class SingletonThing
+    {
+        private static readonly Lazy<SingletonThing> _singetonThing = new Lazy<SingletonThing>(() => new SingletonThing(), LazyThreadSafetyMode.ExecutionAndPublication);
+        private static string _name1;
+        private static string _name2;
+        
+        private SingletonThing()
+        {
+            _name1 = "Chris";
+            _name2 = "Ballance";
+        }
+
+        public static SingletonThing Instance
+        {
+            get
+            {
+                return _singetonThing.Value;
+            }
+        }
+
+        public string Name1
+        {
+            get { return _name1; }
+            set { _name1 = value; }
+        }
+
+        public string Name2
+        {
+            get { return _name2; }
+            set { _name2 = value; }
+        }
+    }
+}
